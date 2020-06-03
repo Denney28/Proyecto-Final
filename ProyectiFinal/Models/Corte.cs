0eca4650ac3e6212386ce059dec1bf5027f70faf
Denney1228 <denney28121996@htomail.com>
2020-06-02 23:46:20 -0700

    PFINAL

---
+++ ProyectiFinal/Models/Corte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace ProyectiFinal.Models
+{
+    public class Corte
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion")]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        public int Price { get; set; }
+    }
+}
